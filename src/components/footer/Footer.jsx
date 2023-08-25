ceff363a21cc9ad39d9b8a395a304b82ed9b8234
Rommel Canoy <48854015+rommelcanoy@users.noreply.github.com>
2023-08-25 08:07:30 +0800

    chore: update info

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -14,19 +14,19 @@ const Footer = () => {
     return (
         <footer className="footer">
             <ContentWrapper>
-                <ul className="menuItems">
+                {/* <ul className="menuItems">
                     <li className="menuItem">Terms Of Use</li>
                     <li className="menuItem">Privacy-Policy</li>
                     <li className="menuItem">About</li>
                     <li className="menuItem">Blog</li>
                     <li className="menuItem">FAQ</li>
-                </ul>
+                </ul> */}
                 <div className="infoText">
                     Welcome to TrailerPlus, your source for captivating movie and TV show trailers. Explore a world of stories, top casts, and thrilling previews that provide a taste of the excitement to come. Immerse yourself in the art of anticipation as you discover a curated selection of trailers that offer a glimpse into the cinematic wonders awaiting you. Lights, camera, anticipation!
                 </div>
 
                 <div className="socialIcons">
-                    <span className="icon">
+                    {/* <span className="icon">
                         <FaFacebookF />
                     </span>
                     <span className="icon">
@@ -34,11 +34,13 @@ const Footer = () => {
                     </span>
                     <span className="icon">
                         <FaTwitter />
-                    </span>
-                    <span className="icon">
+                    </span> */}
+                    {/* <span className="icon">
                         <FaLinkedin />
-                    </span>
+                    </span> */}
                 </div>
+                <div style={{ marginTop: '', marginBottom: '' }}>Created by Rommel Canoy</div>
+
             </ContentWrapper>
         </footer>
     );
